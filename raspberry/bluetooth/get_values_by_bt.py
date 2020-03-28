eb360a1c49aa210dbfff357c7df51632732eac2f
RayK <raymond.kalander@gmail.com>
2020-03-28 13:36:35 +0000

    update python script

--- raspberry/bluetooth/get_values_by_bt.py
+++ raspberry/bluetooth/get_values_by_bt.py
@@ -2,21 +2,23 @@ import serial
 import time
 port = serial.Serial("/dev/rfcomm0", baudrate=9600,timeout=1)
 
-# reading and writing data from and to arduino serially.                                      
+# reading and writing data from and to arduino serially.
 # rfcomm0 -> this could be different
 while True:
-	time.sleep(1)
+	time.sleep(60)
 
         try:
            port.write("read\n");
-           rcv = port.readline()
-	   if rcv:
-	      print("<-- Read from Arduino:" + rcv)
-              out_file = open("/tmp/dht22.out","w+")
-              out_file.write(rcv)
-              out_file.close()
-           else:
-              print("No data..")
+           while True:
+              rcv = port.readline()
+	      if rcv:
+	         print("From Arduino read:" + rcv)
+                 out_file = open("/tmp/dht22.out","w+")
+                 out_file.write(rcv)
+                 out_file.close()
+              else:
+                 print("No data on port...")
+                 break
         except serial.serialutil.SerialException as serial_exception:
            print(serial_exception)
            ## open again...
